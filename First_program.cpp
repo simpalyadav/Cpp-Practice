4ebe48e2ec243c99bf88bd09790a91f2e513578e
simpalyadav <simpal.jnvmau@gmail.com>
2026-08-02 17:35:45 +0530

    Add files via upload

---
+++ First_program.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello coder army" <<endl;
+    cout<<"hello coder army" <<endl ;
+    cout<<"hello coder army" <<endl;
+    
+};
+
+
+
---
+++ Happy_Friendshipday.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+   cout<<"HAPPY FRIENDSHIP DAY DEAR NEETU"<<endl;
+}
