3217615b3a7d0cf0901f7d48b2c82ec661cce5f0
Simpal Yadav <simpal.jnvmau@gmail.com>
2026-08-07 22:19:36 +0530

    Add 19 20 21

---
+++ 20_SquareNumberPattern.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=6;i++)
+    {
+        for(j=1;j<=5;j++)
+        cout<<j*j<<" ";
+        cout<<endl;
+    }
+
+
+
+
+}
---
+++ 21_AlphabetPattern.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+  char i,j;
+  for(i='F';i<='K';i++)
+  {
+    for(j='F';j<='K';j++)
+     cout<<j<<' ';
+     cout<<endl;
+
+  }
+
+
+
+
+
+
+}
